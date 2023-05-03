0f4eb329d596a2ea52c1c67c76d47459e3dc51d6
Mateo Cereceda <70116950+MateoCereceda@users.noreply.github.com>
2023-05-03 11:38:32 -0400

    Fetch de API funcionando
    Creación de una página html básica para comprobar que el fetch de la API funciona.

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const gallery = document.getElementById('gallery');
+
+const apiKey = 'V45jZq6t';
+
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const query = searchInput.value.trim();
+    if (query) {
+        searchArtworks(query);
+    }
+});
+
+async function searchArtworks(query) {
+    const url = `https://www.rijksmuseum.nl/api/en/collection?key=${apiKey}&q=${query}&imgonly=True`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+        const data = await response.json();
+        displayResults(data.artObjects);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+function displayResults(artworks) {
+    gallery.innerHTML = '';
+
+    artworks.forEach((artwork) => {
+        const artDiv = document.createElement('div');
+        artDiv.classList.add('artwork');
+
+        const artImg = document.createElement('img');
+        artImg.src = artwork.webImage.url;
+        artDiv.appendChild(artImg);
+
+        const artTitle = document.createElement('h2');
+        artTitle.textContent = artwork.title;
+        artDiv.appendChild(artTitle);
+
+        const artArtist = document.createElement('p');
+        artArtist.textContent = `Artist: ${artwork.principalOrFirstMaker}`;
+        artDiv.appendChild(artArtist);
+
+        gallery.appendChild(artDiv);
+    });
+}
